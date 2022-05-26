c00f951ea03c34c01faa6b9d094abb7584167895
faizulislamfair <faizulislamfair@gmail.com>
2022-05-26 22:00:30 +0600

    linking between pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,14 @@
+import Link from 'next/link'
+
 const Navbar = () => {
     return (  
         <nav>
             <div className="logo">
                 <h1>Panda List</h1>
             </div>
-            <a>Home</a>
-            <a>About</a>
-            <a>Panda Listing</a>
+            <Link href="/"><a>Home</a></Link>
+            <Link href="/about"><a>About</a></Link>
+            <Link href="/pandas"><a>Panda Listing</a></Link>
         </nav>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -11,6 +12,9 @@ export default function Home() {
       <h1>Home Page</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quia earum quam, commodi non voluptatibus minus temporibus possimus est magnam minima. Iure doloribus explicabo eveniet?</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quia earum quam, commodi non voluptatibus minus temporibus possimus est magnam minima. Iure doloribus explicabo eveniet?</p>
+      <Link href="/pandas">
+        <a>See Panda Listing</a>
+      </Link>
       <Footer />
     </div>
   )
