f706611802f06f4369adc33ac6f79b22e9925f5a
faizulislamfair <faizulislamfair@gmail.com>
2022-05-27 13:17:40 +0600

    redirecting users

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,18 @@
 import Link from 'next/link'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const NotFoundPage = () => {
+
+    const router = useRouter();
+
+    useEffect(() => {
+       setTimeout(() => {
+            //router.go(1)
+            router.push('/');
+       }, 3000)
+    }, [])
+
     return ( 
         <div className="not-found">
             <h1>Oooops...</h1>
